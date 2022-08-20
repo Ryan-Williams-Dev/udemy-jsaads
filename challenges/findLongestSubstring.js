a8cdbcd53eb11e5675d4c87e722a7bf3bc9cbdb1
Ryan Williams <r.d.williams.ca@gmail.com>
2022-08-19 17:37:18 -0700

    solution to findLongestString

--- challenges/findLongestSubstring.js
+++ challenges/findLongestSubstring.js
@@ -1,5 +1,27 @@
-function findLongestSubstring(){
-  
+function findLongestSubstring(str){
+  if (str.length === 0) return 0;
+  if (str.length === 1) return 1;
+
+  let maxWindow = 0;
+  let start = 0;
+  let end = 1;
+  const lookup = new Set();
+
+  lookup.add(str[0])
+
+  while (start < str.length) {
+   if (!lookup.has(str[end]) && str[end] !== undefined) {
+      lookup.add(str[end]);
+      maxWindow = Math.max(lookup.size, maxWindow);
+      end++;
+    } else {
+      maxWindow = Math.max(lookup.size, maxWindow);
+      lookup.delete(str[start]);
+      start++;
+    }
+  }
+
+  return maxWindow;
 }
 
 
