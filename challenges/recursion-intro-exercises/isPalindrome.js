72724c642d07acf5f4984f708a6d5bb1f5e41e9c
Ryan Williams <r.d.williams.ca@gmail.com>
2022-08-20 22:32:10 -0700

    solution to isPalindrome

---
+++ challenges/recursion-intro-exercises/isPalindrome.js
@@ -0,0 +1,16 @@
+console.log(isPalindrome('awesome')) // false
+console.log(isPalindrome('foobar')) // false
+console.log(isPalindrome('tacocat')) // true
+console.log(isPalindrome('amanaplanacanalpanama')) // true
+console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+console.log(isPalindrome('maddam')) // true
+
+function isPalindrome(str){
+  if (str.length <= 1) return true;
+
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1,-1))
+  } 
+
+  return false;
+}
