d202a6dc9e421bb568555ef389f367148a33aed1
Ryan Williams <r.d.williams.ca@gmail.com>
2022-08-24 14:38:44 -0700

    refactored bubble sort

--- challenges/sorting/bubbleSort.js
+++ challenges/sorting/bubbleSort.js
@@ -1,14 +1,20 @@
 function bubbleSort(arr) {
   
-  for (let i = 0; i < arr.length; i++) {
-
-    for (let j = 0; j < arr.length - i - 1; j++) {
+  
+  for (let i = arr.length; i > 0; i--) {
+    let noSwap = true
+    
+    for (let j = 0; j < i - 1; j++) {
       
       if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwap = false;
       }
     }
+
+    if (noSwap) break;
   }
+    
 
   return arr;
 }
