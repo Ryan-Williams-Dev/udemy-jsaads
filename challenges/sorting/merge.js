f6ec782d55862b60ffa24c13aac2a91b36262b49
Ryan Williams <r.d.williams.ca@gmail.com>
2022-08-30 11:57:56 -0700

    working solution, previous one didn't work as intended

--- challenges/sorting/merge.js
+++ challenges/sorting/merge.js
@@ -3,7 +3,7 @@ function merge(arr1, arr2) {
   let i = 0;
   let j = 0;
 
-  while (result.length < arr1.length + arr2.length) {
+  while (i < arr1.length && j < arr2.length) {
     if (arr1[i] < arr2[j]) {
       result.push(arr1[i]);
       i++;
@@ -13,7 +13,7 @@ function merge(arr1, arr2) {
     }
   }
 
-  return result;
+  return result.concat(i < arr1.length ? arr1.slice(i) : arr2.slice(j));
 }
 
-console.log(merge([1,10,50], [2,14,99,100]))
+console.log(merge([1,10,50, 200, 650], [2,14,99,100, 1000, 5000, 3000, 7000, 6505]))
