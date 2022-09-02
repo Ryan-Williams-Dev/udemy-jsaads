3ea79219c2727c1e56cc26663df2e92b865d1df6
Ryan Williams <r.d.williams.ca@gmail.com>
2022-09-01 17:53:26 -0700

    wrote digit count and mostDigits helper functions

--- challenges/sorting/getDigit.js
+++ challenges/sorting/getDigit.js
@@ -6,4 +6,17 @@ function getDigit(num, digit) {
   return result;
 }
 
+function digitCount(num){ 
+  return String(num).length;
+}
+
+function mostDigits(nums) {
+  let max = 0;
+  for (const num of nums) {
+    max = Math.max(digitCount(num), max);
+  }
+  return max;
+}
+
 console.log(getDigit(12345, 0));
+console.log(digitCount(0))
