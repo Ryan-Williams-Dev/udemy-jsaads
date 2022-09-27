a90ac85a1b54be6496ea9e19d40a9cae3dc3578b
Ryan Williams <r.d.williams.ca@gmail.com>
2022-09-27 13:54:17 -0700

    .

--- challenges/findLongestSubstring.js
+++ challenges/findLongestSubstring.js
@@ -1,16 +1,15 @@
 function findLongestSubstring(str){
   if (str.length === 0) return 0;
-  if (str.length === 1) return 1;
 
   let maxWindow = 0;
   let start = 0;
   let end = 1;
   const lookup = new Set();
 
-  lookup.add(str[0])
+  lookup.add(str[0]);
 
   while (start < str.length) {
-   if (!lookup.has(str[end]) && str[end] !== undefined) {
+    if (!lookup.has(str[end]) && str[end] !== undefined) {
       lookup.add(str[end]);
       maxWindow = Math.max(lookup.size, maxWindow);
       end++;
