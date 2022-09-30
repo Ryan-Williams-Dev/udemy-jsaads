7155e45224c9a39821e48dd16b6b8d1a2612e9e2
Ryan Williams <r.d.williams.ca@gmail.com>
2022-09-29 18:59:56 -0700

    node class for sll and push method

---
+++ DS-examples/singly-linked-lists/pushPop.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SingleLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new SingleLinkedList()
+list.push("Hello")
+list.push("Goodbye")
+list.push("Oh")
+list.push("wait")
+
+console.log(list)
