90c7f0b45f3d9cad3ac452179328c025ae402fe7
Ryan Williams <r.d.williams.ca@gmail.com>
2022-10-02 19:54:37 -0700

    unshift

--- DS-examples/singly-linked-lists/examples.js
+++ DS-examples/singly-linked-lists/examples.js
@@ -57,6 +57,21 @@ class SingleLinkedList {
     }
     return removedHead;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
 }
  
 let list = new SingleLinkedList()
